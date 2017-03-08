250e16d206f12f4d0e9c0c1d1c69dfa8ac75464e
xSkullxNInja <trenton360@hotmail.com>
2017-03-07 18:50:06 -0700

    Initial Push. This push contains: 1. Determining Number of Teams 2. Inputing name for each team Errors: 1. Number of teams cannot handle anything but numbers

---
+++ TournamentBracket/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentBracket
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            (new Program()).Run();
+        }
+        public void Run()
+        {
+            Console.WriteLine("NumPlayers");
+            int numPlayers = GetNumPlayers();
+            Console.WriteLine("NumPlayers: " + numPlayers);
+            List<string> players = new List<string>();
+            for(int i = 1; i <= numPlayers; ++i)
+            {
+                Console.WriteLine("Player " + (i) + ": ");
+                players.Add(GetPlayerName());
+            }
+            Console.WriteLine(players.Count);
+            for (int i = 0; i < numPlayers; ++i)
+            {
+                Console.WriteLine(players[i]);
+            }
+        }
+        private int GetNumPlayers()
+        {
+            string numPlayers = Console.ReadLine();
+            return Int32.Parse(numPlayers);
+        }
+        private string GetPlayerName()
+        {
+            return Console.ReadLine();
+        }
+        private void InputPlayer(string playerName)
+        {
+
+        }
+    }
+}
